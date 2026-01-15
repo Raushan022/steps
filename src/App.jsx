84a6233b35280e24dce1150708ff59f6b123759b
raushan <kraushan022@gmail.com>
2026-01-15 16:40:06 +0530

    steps submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import "./App.css";
 
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
+  const [step, setSteps] = useState(1);
+
+  const handlePrevious = () => {
+    if (step > 1) {
+      setSteps(step - 1);
+    }
+  };
+
   return (
     <>
-      <h1>Hello React</h1>
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className="message">
+          Step {step} - {messages[step - 1]}
+        </p>
+
+        <div className="buttons">
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={handlePrevious}
+          >
+            Previous
+          </button>
+          <button
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+            onClick={() => setSteps(step < 3 ? step + 1 : step)}
+          >
+            Next
+          </button>
+        </div>
+      </div>
     </>
   );
 }
